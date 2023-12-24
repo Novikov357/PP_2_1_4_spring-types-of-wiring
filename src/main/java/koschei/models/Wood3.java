2bd87832e2fdca89f3dc4416d2299019f461fad3
Alexey Novikov <wasvend@gmail.com>
2023-12-24 22:58:33 +0300

    Add rabbit, duck, egg, needle, death to chain

--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class Wood3 {
 
-    private final Rabbit4 rabbit;
+    private final Rabbit4 RABBIT;
 
     @Autowired
     public Wood3(Rabbit4 rabbit) {
-        this.rabbit = rabbit;
+        this.RABBIT = rabbit;
     }
 
     @Override
     public String toString() {
-        return ", на дереве заяц " + rabbit.toString();
+        return ", на дереве заяц " + RABBIT.toString();
     }
 }
